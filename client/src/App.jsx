118620bb83e309c8104bc2c79615f57c24fc29ca
GentilleauF <francois.gentilleau@gmail.com>
2024-02-21 16:54:08 +0100

    ajout style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App({ socket }) {
   const [author, setAuthor] = useState(null);
-  const [messages,setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
 
   function sendMessage(event) {
@@ -21,33 +21,41 @@ function App({ socket }) {
     const author = event.target.querySelector("#author").value;
     setAuthor(author);
 
-    socket.on("new_msg", (newMessage) =>{
-      setMessages((prevMessages) =>[
+    socket.on("new_msg", (newMessage) => {
+      setMessages((prevMessages) => [
         ...prevMessages, newMessage
       ]);
     });
   }
 
 
-  const messageElements = messages.map((message,i)=>{
+  const messageElements = messages.map((message, i) => {
     const date = (new Date(message.date)).toUTCString();
     return (
-    <div key={i} className="message">
-    <p>{message.content}</p>
-    <p>{message.author}</p>
-    <p>{date}</p>
-    </div>
+      <div key={i} className="message">
+        <p>{message.content}</p>
+        <p>{message.author}</p>
+        <p>{date}</p>
+      </div>
     );
-    });
+  });
 
 
   return (
-    <div>
-      <h1>Client Chat</h1>
-      <form onSubmit={handleConnexion} hidden={author != null}>
-        <input type="text" id="author" />
-        <button >Se connecter</button>
-      </form>
+    <div className='min-h-screen bg-slate-500'>
+      <h1 className='text-xl text-center font-medium p-10'>Client Chat</h1>
+      <div className='bg-slate-200 rounded absolute top-20 inset-x-1/4 h-60 ' hidden={author != null}>
+        <form className='flex flex-col ' onSubmit={handleConnexion} >
+          <div>
+            <h3 className='text-center m-10 font-medium'>Formulaire de Connexion</h3>
+            <div className='flex justify-center item-center'>
+              <input className='mr-5' type="text" id="author" />
+              <button className='bg-blue-400 p-1 px-3 rounded' >Se connecter</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
       <form onSubmit={sendMessage} hidden={author == null}>
         <input type="text" id="message" />
         <button>Envoyer</button>
--- client_lan/src/main.jsx
+++ client_lan/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import {io} from "socket.io-client";
 
 
-const socket = io("localhost:3000");
+const socket = io("172.16.6.1:3000");
 
 // const msg = {
 //   content : "Mon message",
